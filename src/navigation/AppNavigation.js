6037fd70e7c3ea3e84e1f97914833e282f4ec63d
Toni Cordon <toni.cordon@koobin.com>
2022-09-21 23:51:41 +0200

    feat: adding icons to BottomTabNavigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Icon } from '@rneui/base'
 
 import {
   AccountScreen,
@@ -9,10 +10,48 @@ import {
 } from '../screens'
 
 const Tab = createBottomTabNavigator()
+const tabBarIconOptions = (routeName, focused, color, size) => {
+  const iconName = {
+    focused: {
+      Restaurant: 'compass',
+      Favorites: 'heart',
+      Search: 'feature-search',
+      Ranking: 'star',
+      Account: 'account',
+      Default: 'home'
+    },
+    nonFocused: {
+      Restaurant: 'compass-outline',
+      Favorites: 'heart-outline',
+      Search: 'feature-search-outline',
+      Ranking: 'star-outline',
+      Account: 'account-outline',
+      Default: 'home-outline'
+    }
+  }
+
+  const isFocused = focused ? 'focused' : 'nonFocused'
+  const name = iconName[isFocused][routeName || 'Default']
+  return (
+    <Icon
+      type='material-community'
+      name={name}
+      color={color}
+      size={size}
+    />
+  )
+}
 
 const AppNavigation = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={({ route: { name: routeName } }) => (
+      {
+        tabBarActiveTintColor: '#0000FF',
+        tabBarInactiveTintColor: '#646464',
+        tabBarIcon: ({ focused, color, size }) => tabBarIconOptions(routeName, focused, color, size)
+      }
+    )}
+    >
       <Tab.Screen name='Search' component={SearchScreen} />
       <Tab.Screen name='Ranking' component={RankingScreen} />
       <Tab.Screen name='Restaurant' component={RestaurantsScreen} />
