3da8ab052098db4f133c466c10ee388275504057
Toni Cordon <toni.cordon@koobin.com>
2022-09-29 10:57:23 +0200

    Feat: Controlling Form Data with Formik

---
+++ src/components/Auth/RegisterForm/RegisterForm.data.js
@@ -0,0 +1,7 @@
+export const initialValues = () => {
+  return {
+    email: '',
+    password: '',
+    repeatPassword: ''
+  }
+}
--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,30 +1,47 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Input, Icon, Button } from '@rneui/base'
+import { useFormik } from 'formik'
 
 import { style } from './RegisterForm.styles'
+import { initialValues } from './RegisterForm.data'
 
 export function RegisterForm () {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    onSubmit: (formValue) => {
+      console.log('Form Enviado')
+      console.log(formValue)
+    }
+  })
   return (
     <View style={style.content}>
       <Input
         placeholder='Correo electrónico'
         containerStyle={style.input}
         rightIcon={<Icon type='material-community' name='at' iconStyle={style.icon} />}
+        onChangeText={text => formik.setFieldValue('email', text)}
       />
       <Input
         secureTextEntry
         placeholder='Contraseña'
         containerStyle={style.input}
         rightIcon={<Icon type='material-community' name='eye-outline' iconStyle={style.icon} />}
+        onChangeText={text => formik.setFieldValue('password', text)}
       />
       <Input
         secureTextEntry
         placeholder='Repetir contraseña'
         containerStyle={style.input}
         rightIcon={<Icon type='material-community' name='eye-outline' iconStyle={style.icon} />}
+        onChangeText={text => formik.setFieldValue('repeatPassword', text)}
       />
-      <Button title='Unirse' containerStyle={style.buttonContainer} buttonStyle={style.button} />
+      <Button
+        title='Unirse'
+        containerStyle={style.buttonContainer}
+        buttonStyle={style.button}
+        onPress={formik.handleSubmit}
+      />
     </View>
   )
 }
