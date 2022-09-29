1c4e09233590d46171ba3b9e5016492c68843a62
Toni Cordon <toni.cordon@koobin.com>
2022-09-29 11:06:01 +0200

    Feat: Validation Formik Form with Yup

--- src/components/Auth/RegisterForm/RegisterForm.data.js
+++ src/components/Auth/RegisterForm/RegisterForm.data.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup'
+
 export const initialValues = () => {
   return {
     email: '',
     password: '',
     repeatPassword: ''
   }
+}
+
+export const validationSchema = () => {
+  return Yup.object({
+    email: Yup.string()
+      .email('El email no es correcto')
+      .required('El email es obligatorio'),
+    password: Yup.string()
+      .required('La contraseña es obligatoria'),
+    repeatPassword: Yup.string()
+      .required('La contraseña es obligatoria')
+      .oneOf([Yup.ref('password')], 'Las contraseñas no coinciden')
+  })
 }
--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -4,11 +4,13 @@ import { Input, Icon, Button } from '@rneui/base'
 import { useFormik } from 'formik'
 
 import { style } from './RegisterForm.styles'
-import { initialValues } from './RegisterForm.data'
+import { initialValues, validationSchema } from './RegisterForm.data'
 
 export function RegisterForm () {
   const formik = useFormik({
     initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
     onSubmit: (formValue) => {
       console.log('Form Enviado')
       console.log(formValue)
@@ -21,6 +23,7 @@ export function RegisterForm () {
         containerStyle={style.input}
         rightIcon={<Icon type='material-community' name='at' iconStyle={style.icon} />}
         onChangeText={text => formik.setFieldValue('email', text)}
+        errorMessage={formik.errors.email}
       />
       <Input
         secureTextEntry
@@ -28,6 +31,7 @@ export function RegisterForm () {
         containerStyle={style.input}
         rightIcon={<Icon type='material-community' name='eye-outline' iconStyle={style.icon} />}
         onChangeText={text => formik.setFieldValue('password', text)}
+        errorMessage={formik.errors.password}
       />
       <Input
         secureTextEntry
@@ -35,6 +39,7 @@ export function RegisterForm () {
         containerStyle={style.input}
         rightIcon={<Icon type='material-community' name='eye-outline' iconStyle={style.icon} />}
         onChangeText={text => formik.setFieldValue('repeatPassword', text)}
+        errorMessage={formik.errors.repeatPassword}
       />
       <Button
         title='Unirse'
