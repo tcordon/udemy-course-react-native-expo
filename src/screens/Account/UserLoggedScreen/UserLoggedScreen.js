1e1509fd53b95ad6127be6c456e1c4725cbc96ad
Toni Cordon <toni.cordon@koobin.com>
2022-10-03 16:24:06 +0200

    Feat: Session Close Button

--- src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
+++ src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { Button } from '@rneui/base'
+import { getAuth, signOut } from 'firebase/auth'
+
 import { InfoUser } from '../../../components/Account'
-import { style } from '../../../components/Auth/LoginForm/LoginForm.styles'
+import { style } from './UserLoggedScreen.styles'
 
 export function UserLoggedScreen () {
+  const logout = async () => {
+    const auth = getAuth()
+    await signOut(auth)
+  }
   return (
     <View>
       <InfoUser />
+
+      <Button
+        title='Cerrar Sessión'
+        buttonStyle={style.button}
+        titleStyle={style.buttonTitle}
+        onPress={logout}
+      />
     </View>
   )
 }
--- src/screens/Account/UserLoggedScreen/UserLoggedScreen.styles.js
+++ src/screens/Account/UserLoggedScreen/UserLoggedScreen.styles.js
@@ -1,4 +1,17 @@
 import { StyleSheet } from 'react-native'
 
 export const style = StyleSheet.create({
+  button: {
+    marginTop: 30,
+    paddingVertical: 10,
+    borderRadius: 0,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#e3e3e3',
+    borderBottomWidth: 1,
+    borderBottomColor: '#e3e3e3'
+  },
+  buttonTitle: {
+    color: '#00a680'
+  }
 })
