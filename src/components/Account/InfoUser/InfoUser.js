7cc10f73191ff8724beb7633e09a26e380cb2f44
Toni Cordon <toni.cordon@koobin.com>
2022-10-03 23:38:03 +0200

    Feat: Preparing Firebase's Image Upload

--- src/components/Account/InfoUser/InfoUser.js
+++ src/components/Account/InfoUser/InfoUser.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { Avatar } from '@rneui/base'
 import { getAuth } from 'firebase/auth'
+import * as ImagePicker from 'expo-image-picker'
 
 import { style } from './InfoUser.styles'
 
 export function InfoUser () {
   const { uid, photoUrl, displayName, email } = getAuth().currentUser
 
-  const changeAvatar = () => {
-    console.log('change avatar')
+  const changeAvatar = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3]
+    })
+
+    if (!result.cancelled) uploadImage(result.uri)
   }
+
+  const uploadImage = (uri) => {
+    console.log(uri)
+  }
+
   return (
     <View style={style.content}>
       <Avatar
