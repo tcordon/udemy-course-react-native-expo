5e6e97dd69137be08989f5644d66f05b5dd20736
Toni Cordon <toni.cordon@koobin.com>
2022-10-07 12:37:38 +0200

    Feat: Validation User Change Name Form

--- src/components/Account/ChangeDisplayNameForm/ChangeDisplayNameForm.js
+++ src/components/Account/ChangeDisplayNameForm/ChangeDisplayNameForm.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Input, Button } from '@rneui/base'
+import { useFormik } from 'formik'
 
 import { styles } from './ChangeDisplayNameForm.styles'
+import { initialValues, validationSchema } from './ChangeDisplayNameForm.data'
 
 export function ChangeDisplayNameForm () {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log(formValue)
+    }
+  })
+
   return (
     <View style={styles.viewContent}>
       <Input
@@ -14,11 +25,15 @@ export function ChangeDisplayNameForm () {
           name: 'account-circle-outline',
           color: '#c2c2c2'
         }}
+        onChangeText={(text) => formik.setFieldValue('displayName', text)}
+        errorMessage={formik.errors.displayName}
       />
       <Button
         title='Cambiar Nombre y Apellidos'
         containerStyle={styles.btnContainer}
         buttonStyle={styles.btn}
+        onPress={formik.handleSubmit}
+        loading={formik.isSubmitting}
       />
     </View>
   )
