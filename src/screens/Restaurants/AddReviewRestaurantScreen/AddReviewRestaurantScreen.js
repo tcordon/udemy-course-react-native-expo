6cf353d746581508390b6d1f49aa1e3b3c6b0b8c
Toni Cordon <toni.cordon@koobin.com>
2022-10-19 20:05:51 +0200

    Feat: Add Reviw Form

--- src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
+++ src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { AirbnbRating, Input, Button } from '@rneui/base'
 
 import { style } from './AddReviewRestaurantScreen.styles'
 
 export function AddReviewRestaurantScreen (props) {
   const { route } = props
-  console.log(route.params)
+
   return (
-    <View>
-      <Text>AddReviewRestaurantScreen</Text>
+    <View style={style.content}>
+      <View>
+        <View style={style.ratingContent}>
+          <AirbnbRating
+            count={5}
+            reviews={[
+              'Pésimo',
+              'Deficiente',
+              'Normal',
+              'Muy Bueno',
+              'Excelente'
+            ]}
+            defaultRating={0}
+            size={35}
+            onFinishRating={(rating) => console.log(rating)}
+          />
+        </View>
+        <View>
+          <Input placeholder='Title' />
+          <Input
+            placeholder='Comentario'
+            multiline
+            inputStyle={style.comment}
+          />
+        </View>
+      </View>
+      <Button
+        title='Enviar Review'
+        containerStyle={style.btnContainer}
+        buttonStyle={style.btn}
+      />
     </View>
   )
 }
