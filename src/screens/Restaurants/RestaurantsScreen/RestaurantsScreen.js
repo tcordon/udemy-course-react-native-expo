f5012887ec2950abd7c71ccfdce8706b2a979a4d
Toni Cordon <toni.cordon@koobin.com>
2022-10-19 12:20:15 +0200

    Feat: Get Restaurants

--- src/screens/Restaurants/RestaurantsScreen/RestaurantsScreen.js
+++ src/screens/Restaurants/RestaurantsScreen/RestaurantsScreen.js
@@ -2,18 +2,32 @@ import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import { Icon } from '@rneui/base'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { collection, Collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 
 import { screen } from '../../../data/screensData'
 import { style } from './RestaurantsScreen.styles'
+import { db } from '../../../data/firebase'
 
 export const RestaurantsScreen = (props) => {
   const { navigation } = props
   const [currentUser, setCurrentUser] = useState(null)
+  const [restaurants, setRestaurants] = useState(null)
 
   useEffect(() => {
     const auth = getAuth()
     onAuthStateChanged(auth, (user) => {
       setCurrentUser(user)
+    })
+  }, [])
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'restaurants'),
+      orderBy('createdAt', 'desc')
+    )
+
+    onSnapshot(q, (snapshot) => {
+      setRestaurants(snapshot.docs)
     })
   }, [])
 
