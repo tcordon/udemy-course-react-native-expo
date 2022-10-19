61cd82a74a874b4e1e8a738756f0204302ccd4c0
Toni Cordon <toni.cordon@koobin.com>
2022-10-19 18:13:28 +0200

    Feat: Open Map

--- src/components/Shared/Map/Map.js
+++ src/components/Shared/Map/Map.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
+import OpenMap, { createOpenLink } from 'react-native-open-maps'
 
 import { style } from './Map.styles'
 
 export function Map (props) {
   const { location, name } = props
+  const openAppMap = createOpenLink(
+    {
+      latitude: location.latitude,
+      longitude: location.longitude,
+      zoom: 19,
+      query: name
+    }
+  )
+
   return (
     <MapView
       initialRegion={location}
       style={style.content}
+      onPress={openAppMap}
+      zoom={19}
     >
       <Marker coordinate={location} />
     </MapView>
