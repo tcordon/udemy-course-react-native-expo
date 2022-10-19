59d5ec23c155bbc217c6beca4529563a15858f0a
Toni Cordon <toni.cordon@koobin.com>
2022-10-19 13:12:40 +0200

    Feat: Get Restaurant data from Firebase/firestore

--- src/screens/Restaurants/RestaurantScreen/RestaurantScreen.js
+++ src/screens/Restaurants/RestaurantScreen/RestaurantScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { doc, collection, query, where, orderBy, onSnapshot } from 'firebase/firestore'
 
 import { style } from './RestaurantScreen.styles'
+import { db } from '../../../data/firebase'
 
 export function RestaurantScreen (props) {
   const { route } = props
-  console.log(route)
+  const [restaurant, setRestaurant] = useState(null)
+
+  useEffect(() => {
+    setRestaurant(null)
+
+    onSnapshot(doc(db, 'restaurants', route.params.id), (doc) => {
+      setRestaurant(doc.data())
+    })
+  }, [route.params.id])
+
   return (
     <View>
       <Text>RestaurantScreen</Text>
