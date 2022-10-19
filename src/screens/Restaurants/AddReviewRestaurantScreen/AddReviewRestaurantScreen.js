e6876047b84ae2afaba31775c78b17594def615a
Toni Cordon <toni.cordon@koobin.com>
2022-10-19 20:29:21 +0200

    FEat: Save Restaurant Review

--- src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
+++ src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { View } from 'react-native'
 import { AirbnbRating, Input, Button } from '@rneui/base'
+import Toast from 'react-native-toast-message'
+import 'react-native-get-random-values'
+import { v4 as uuid } from 'uuid'
+import { getAuth } from 'firebase/auth'
+import { doc, setDoc, query, collection, where, onSnapshot, updateDoc } from 'firebase/firestore'
+import { db } from '../../../data/firebase'
 
 import { style } from './AddReviewRestaurantScreen.styles'
 import { useFormik } from 'formik'
@@ -8,12 +14,34 @@ import { initialValues, validationSchema } from './AddReviewRestaurantScreen.dat
 
 export function AddReviewRestaurantScreen (props) {
   const { route } = props
+  const { params } = route
   const formik = useFormik({
     initialValues: initialValues(),
     validationSchema: validationSchema(),
     validateOnChange: false,
     onSubmit: async (formValue) => {
-      console.log(formValue)
+      try {
+        const auth = getAuth()
+        const idDoc = uuid()
+        const newData = formValue
+        newData.id = idDoc
+        newData.idRestaurant = params.idRestaurant
+        newData.idUser = auth.currentUser.uid
+        newData.avatar = auth.currentUser.photoURL
+        newData.createdAt = new Date()
+
+        await setDoc(
+          doc(db, 'reviews', idDoc),
+          newData
+        )
+      } catch (error) {
+        Toast.show({
+          type: 'error',
+          position: 'bottom',
+          text1: 'Error al enviar la review'
+        })
+        console.log(error)
+      }
     }
   })
 
