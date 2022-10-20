15a0e0fc0fc58bf7b062834f5c865e3f7c625078
Toni Cordon <toni.cordon@koobin.com>
2022-10-20 10:44:21 +0200

    Feat: Get user favorites

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { style } from './Favorites.styles'
+
+export function Favorites () {
+  return (
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  )
+}
--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-
+import { doc, getDoc, collection, query, where, onSnapshot } from 'firebase/firestore'
 import { UserNotLogged } from '../components/Favorites/UserNotLogged'
+
+import { db } from '../data/firebase'
 
 export const FavoritesScreen = () => {
   const [hasLogged, setHasLogged] = useState(null)
+  const [restaurants, setRestaurants] = useState([])
   const auth = getAuth()
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setHasLogged(user)
+    })
+  }, [])
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'favorites'),
+      where('idUser', '==', auth.currentUser.uid)
+    )
+    onSnapshot(q, async (snapshot) => {
+      let restaurantArray = []
+      for await (const item of snapshot.docs) {
+        const data = item.data()
+        const docRef = doc(db, 'restaurants', data.idRestaurant)
+        const docSnap = await getDoc(docRef)
+        const newData = docSnap.data()
+        newData.idFavorite = data.id
+        restaurantArray.push(newData)
+      }
+      setRestaurants(restaurantArray)
     })
   }, [])
 
