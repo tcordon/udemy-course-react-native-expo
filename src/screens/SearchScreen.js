2610c0b69a48f302ce87bd26d01a579a94df2da5
Toni Cordon <toni.cordon@koobin.com>
2022-10-20 14:21:09 +0200

    Feat: Search Screen Search Bar and loading

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, ScrollView } from 'react-native'
+import { Text, SearchBar, ListItem, Avatar, Icon } from '@rneui/base'
+import { Loading } from '../components/Shared'
 
 export const SearchScreen = () => {
+  const [searchText, setSearchText] = useState('')
+  const [searchResults, setSearchResults] = useState(null)
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <>
+      <SearchBar
+        placeholder='Busca tu restaurante'
+        onChangeText={(text) => setSearchText(text)}
+        value={searchText}
+      />
+      {!searchResults && <Loading show text='Cargando...' />}
+    </>
   )
 }
