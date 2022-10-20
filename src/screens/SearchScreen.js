0f9ed8f03226f0d3a336edaf1ffefc1fc893269e
Toni Cordon <toni.cordon@koobin.com>
2022-10-20 14:51:06 +0200

    Feat: List filtered restaurants

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { View, ScrollView } from 'react-native'
 import { Text, SearchBar, ListItem, Avatar, Icon } from '@rneui/base'
 import { query, startAt, endAt, limit, orderBy, getDocs, collection } from 'firebase/firestore'
-import { size } from 'lodash'
+import { size, map } from 'lodash'
 
 import { Loading } from '../components/Shared'
 import { db } from '../data/firebase'
@@ -10,8 +10,6 @@ import { db } from '../data/firebase'
 export const SearchScreen = () => {
   const [searchText, setSearchText] = useState('')
   const [searchResults, setSearchResults] = useState(null)
-
-  console.log(searchResults[0].data())
 
   useEffect(() => {
     (async () => {
@@ -27,6 +25,10 @@ export const SearchScreen = () => {
     })()
   }, [searchText])
 
+  const goToRestaurant = () => {
+    console.log('navigate to restaurant...')
+  }
+
   return (
     <>
       <SearchBar
@@ -35,6 +37,41 @@ export const SearchScreen = () => {
         value={searchText}
       />
       {!searchResults && <Loading show text='Cargando...' />}
+      <ScrollView>
+        {
+          size(searchResults) === 0
+            ? (
+              <View style={{ alignItems: 'center', marginTop: 20 }}>
+                <Text>No se han encontrado resultados</Text>
+              </View>
+              )
+            : (
+                map(searchResults, (item) => {
+                  const data = item.data()
+                  return (
+                    <ListItem
+                      key={data.id}
+                      bottomDivider
+                      onPress={() => console.log('Go to restaurant')}
+                    >
+                      <Avatar
+                        source={{ uri: data.images[0] }}
+                        rounded
+                      />
+                      <ListItem.Content>
+                        <ListItem.Title>{data.name}</ListItem.Title>
+                      </ListItem.Content>
+                      <Icon
+                        type='material-community'
+                        name='chevron-right'
+                        onPress={goToRestaurant}
+                      />
+                    </ListItem>
+                  )
+                })
+              )
+        }
+      </ScrollView>
     </>
   )
 }
