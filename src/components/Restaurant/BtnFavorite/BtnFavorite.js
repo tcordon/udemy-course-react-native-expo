457ff9bff8b46f5f7b131d8e37169e552c3af7f5
Toni Cordon <toni.cordon@koobin.com>
2022-10-20 09:49:21 +0200

    Feat: Add restaurants to favorites collection

--- src/components/Restaurant/BtnFavorite/BtnFavorite.js
+++ src/components/Restaurant/BtnFavorite/BtnFavorite.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { Icon } from '@rneui/base'
+import { getAuth } from 'firebase/auth'
+import { doc, getDocs, query, where, collection, deleteDoc, setDoc } from 'firebase/firestore'
+import 'react-native-get-random-values'
+import { v4 as uuid } from 'uuid'
 
 import { style } from './BtnFavorite.styles'
+import { db } from '../../../data/firebase'
 
 export function BtnFavorite (props) {
   const { idRestaurant } = props
+  const auth = getAuth()
 
-  const addFavorite = () => {
-    console.log('añadir a favoritos')
+  const addFavorite = async () => {
+    try {
+      // generat uuid for favorite
+      const idFavorite = uuid()
+
+      // Build document
+      const data = {
+        id: idFavorite,
+        idRestaurant,
+        idUser: auth.currentUser.uid
+      }
+
+      // Store document
+      await setDoc(
+        doc(db, 'favorites', idFavorite),
+        data
+      )
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
