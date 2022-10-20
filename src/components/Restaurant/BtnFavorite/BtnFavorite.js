0b7105e7d7fcb424d8d3de1c04b2be19523f837a
Toni Cordon <toni.cordon@koobin.com>
2022-10-20 10:07:40 +0200

    Feat: Get Restaurant favorites

--- src/components/Restaurant/BtnFavorite/BtnFavorite.js
+++ src/components/Restaurant/BtnFavorite/BtnFavorite.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 import { Icon } from '@rneui/base'
 import { getAuth } from 'firebase/auth'
 import { doc, getDocs, query, where, collection, deleteDoc, setDoc } from 'firebase/firestore'
 import 'react-native-get-random-values'
 import { v4 as uuid } from 'uuid'
+import { size } from 'lodash'
 
 import { style } from './BtnFavorite.styles'
 import { db } from '../../../data/firebase'
 
 export function BtnFavorite (props) {
   const { idRestaurant } = props
+  const [isFavorite, setIsFavorite] = useState(undefined)
   const auth = getAuth()
+
+  useEffect(() => {
+    (async () => {
+      const response = await getFavorites()
+      if (size(response)) {
+        setIsFavorite(true)
+      } else {
+        setIsFavorite(false)
+      }
+    })()
+  }, [idRestaurant])
+
+  const getFavorites = async () => {
+    const q = query(
+      collection(db, 'favorites'),
+      where('idRestaurant', '==', idRestaurant),
+      where('idUser', '==', auth.currentUser.uid)
+    )
+    const result = await getDocs(q)
+    return result.docs
+  }
 
   const addFavorite = async () => {
     try {
@@ -35,15 +58,22 @@ export function BtnFavorite (props) {
     }
   }
 
+  const removeFavorite = () => {
+    console.log('delete de favoritos')
+  }
+
   return (
     <View style={style.content}>
-      <Icon
-        type='material-community'
-        name='heart-outline'
-        color='#c2c2c2'
-        size={35}
-        onPress={addFavorite}
-      />
+      {
+        isFavorite !== undefined &&
+          <Icon
+            type='material-community'
+            name={isFavorite ? 'heart' : 'heart-outline'}
+            color={isFavorite ? '#ff0000' : '#c2c2c2'}
+            size={35}
+            onPress={isFavorite ? removeFavorite : addFavorite}
+          />
+      }
     </View>
   )
 }
